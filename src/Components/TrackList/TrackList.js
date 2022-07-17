19831ca633f39b12ac0fd57e04990a7e9ee14ceb
Natsinee Tanapornrat <ploy.natsinee@gmail.com>
2022-07-17 21:50:56 +0700

    begin54.13

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -7,7 +7,6 @@ class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
-        
         {
            this.props.tracks.map(track => {
             return <Track track={track}
