906ceec1b3a15d9b58cb97b96d09dab89df6997e
Natsinee Tanapornrat <ploy.natsinee@gmail.com>
2022-07-20 20:19:41 +0700

    66

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -23,10 +23,12 @@ class Playlist extends React.Component {
                 <TrackList tracks={this.props.playlistTracks} 
                            onRemove={this.props.onRemove} 
                            isRemoval={true}/>
-                <button className="Playlist-save">SAVE TO SPOTIFY</button>
+                <button className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</button>
             </div>
         )
     }
 }
+
+
 
 export default Playlist;
