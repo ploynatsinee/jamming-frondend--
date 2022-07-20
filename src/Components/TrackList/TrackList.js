ddc99cb3e9451a4ccd4fba3da01984ca5ae98eed
Natsinee Tanapornrat <ploy.natsinee@gmail.com>
2022-07-20 15:57:07 +0700

    addremove

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Track from "../Track/Track";
-import "./TrackList.css"
-
+import "./TrackList.css";
 
 class TrackList extends React.Component {
-    render() {
-        return (
-            <div className="TrackList">
-        {
-           this.props.tracks.map(track => {
-            return <Track track={track}
-                      key={track.id}  />
-           }) 
-        }
-        </div>
-        )
-    }
+  render() {
+    return (
+      <div className="TrackList">
+        {this.props.tracks &&
+          this.props.tracks.length > 0 &&
+          this.props.tracks.map((track) => {
+            return (
+              <Track track={track} key={track.id} onAdd={this.props.onAdd} />
+            );
+          })}
+      </div>
+    );
+  }
 }
 export default TrackList;
